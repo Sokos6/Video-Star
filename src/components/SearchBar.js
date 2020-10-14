bec51159fe82bf56600b3f4d3d74dddc9c883fb3
Sokos6 <will.sokolowski@gmail.com>
2020-10-14 14:31:13 -0500

    working term submit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-  state = {
-    term: '',
+  state = { term: ''};
+
+  onInputChange = (event) => {
+    this.setState({ term: event.target.value });
   };
 
-  onInputChange = () => {
+  onFormSubmit = (event) => {
+    event.preventDefault();
 
+    this.props.onFormSubmit(this.state.term);
   }
+
 
   render() {
     return (
       <div className='search-bar ui segment'>
-        <form className='ui form'>
-          <label>Video Star</label>
-          <input type='text' value={this.state.term} onChange={this.onInputChange}/>
+        <form onSubmit={this.onFormSubmit}className='ui form'>
+          <div className='field'>
+            <label>Video Star</label>
+            <input type='text' value={this.state.term} onChange={this.onInputChange}/>
+          </div>
         </form>
       </div>
     );
